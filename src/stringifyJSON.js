fcfabc73d9a792121c4dc81c658f98f68b0ecea9
Marissa <marissa.shieh@gmail.com>
2019-01-31 21:45:21 -0500

    Can now stringifyJSON arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,34 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	var keysArr = [];
+	var valuesArr = [];
+	var returnedItem = "";
+
+	if (typeof obj === "number" || typeof obj === "boolean"){ 
+		return obj.toString();
+	} 
+  else if (typeof obj === "string"){
+    return '"' + obj + '"'
+  }
+
+	else if (obj === NaN || obj === null || obj === Infinity){
+		return null;
+	}
+	else if (Array.isArray(obj)){
+    if (obj[0] === undefined){
+      return '[]';
+    } 
+    else {
+      obj.forEach(function(ele){
+        valuesArr.push(stringifyJSON(ele));
+      });
+    }
+	}
+  return '[' + valuesArr + ']';
 };
+
+	// } else if
+	// else if (typeof obj === "object"){
+	// 	return objString + objArr + "'}'";
+	// }
