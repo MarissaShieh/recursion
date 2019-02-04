07b32e8aa7dcbe7e6396f41f1a49c287f05f0454
Marissa <marissa.shieh@gmail.com>
2019-02-03 19:09:52 -0800

    getElementsByClassName has helper function to do recursion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,18 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+	var foundEle = [];
+	var traverseNodes = function(node){
+		if (node.classList && node.classList.contains(className)){
+			foundEle.push(node)
+		}
+		if (node.childNodes){
+			node.childNodes.forEach(function(innerNode){
+				traverseNodes(innerNode);
+			});
+		}
+	}
+	traverseNodes(document.body);	
+	return foundEle;
 };
